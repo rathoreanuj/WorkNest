e523aede3be0409ae29c4cbb10db234cc3d5a0ec
Anuj Rathore <133572933+rathoreanuj@users.noreply.github.com>
2025-07-27 14:45:53 +0530

    Update InviteUser.tsx

--- client/src/page/invite/InviteUser.tsx
+++ client/src/page/invite/InviteUser.tsx
@@ -60,16 +60,16 @@ const InviteUser = () => {
           className="flex items-center gap-2 self-center font-medium"
         >
           <Logo />
-          Team Sync.
+         WorkNest
         </Link>
         <div className="flex flex-col gap-6">
           <Card>
             <CardHeader className="text-center">
               <CardTitle className="text-xl">
-                Hey there! You're invited to join a TeamSync Workspace!
+                Hey there! You're invited to join a WorkNest Workspace!
               </CardTitle>
               <CardDescription>
-                Looks like you need to be logged into your TeamSync account to
+                Looks like you need to be logged into your WorkNest account to
                 join this Workspace.
               </CardDescription>
             </CardHeader>
